ec587cc8107e37a570205a1fc0e8abb7f67d547b
EMilborn <emilborn@stuy.edu>
2015-11-16 23:15:39 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -38,7 +38,7 @@ public class Warrior extends Character {
     }
     
     public string about(){
-      return "Me warrior varry strong, knot sew smart"
+      return "Me warrior varry strong, but knot sew smart. Pick me if you want to bash in som monster skulls."
     }
 
 }//end class Warrior
