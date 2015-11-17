c912496a4b994d952d5e76aaae4392b0f2810d72
ShaAli <sali1@stuy.edu>
2015-11-16 23:17:36 -0500

    Update CSTeacher.java

--- CSTeacher.java
+++ CSTeacher.java
@@ -1,1 +1,31 @@
+/*=============================================
+  class CSTeacher -- protagonist of Ye Olde RPG
+  =============================================*/
+ 
+public class CSTeacher extends Character {
+
+    /*=============================================
+      default constructor
+      pre:  instance vars are declared
+      post: initializes instance vars.
+      =============================================*/
+    public CSTeacher() {
+	_hitPts = 999;
+	_strength = 999;
+	_defense = 40;
+	_attack = .4;
+    }
+
+
+    /*=============================================
+      overloaded constructor
+      pre:  instance vars are declared
+      post: initializes instance vars. _name is set to input String.
+      =============================================*/
+    public CSTeacher( String name ) {
+	this();
+	_name = name;
+    }
+
+}//end class CSTeacher
 
